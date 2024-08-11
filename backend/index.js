090d2e4dbafc0ca22b53d074adfdc66fcee5a6d6
Amit Kumar <amitkrmaurya09@gmail.com>
2024-08-11 12:57:40 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../syop-fronted/dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT||5000, () => {
     console.log(`Server is running on port ${PORT}`);
 });
