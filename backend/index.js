a63337ed108da146dfad0ba65acb284914f34a93
Amit Kumar <amitkrmaurya09@gmail.com>
2024-08-11 13:10:31 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,7 @@
+import express from 'express'
 import path from 'path';
-import { fileURLToPath } from 'url';
-import express from 'express';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 // Serve static files from the Vite build
@@ -21,6 +17,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../syop-fronted/dist', 'index.html'));
 });
 
-app.listen(PORT||5000, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
