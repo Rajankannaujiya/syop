1f9d40db150188d05b88ed1f9ace170550a58d55
enthusiast <rajankannaujiya2034@gmail.com>
2024-08-15 17:47:09 +0530

    userAuthentication + Prisma

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,23 @@
 import express from 'express'
+import {user} from './components/auth.js'
+import cors from 'cors'
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+
+app.use("/api/v1/user",user)
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
